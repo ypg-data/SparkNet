3ff44506dd3f17b6af99299cd0163171fba80a8a
Robert Nishihara <robertnishihara@gmail.com>
2015-12-12 00:06:34 -0500

    testing

--- src/main/scala/apps/CifarLMDBApp.scala
+++ src/main/scala/apps/CifarLMDBApp.scala
@@ -57,7 +57,8 @@ object CifarLMDBApp {
 
     var netWeights = net.getWeights()
 
-    val workers = sc.parallelize(Array.range(0, numWorkers), numWorkers)
+    //val workers = sc.parallelize(Array.range(0, numWorkers), numWorkers)
+    val workers = sc.parallelize(Array.range(0, 100000)).coalesce(numWorkers).mapPartitions(_ => Array(0).iterator)
 
     // TODO: should get the size from the database instead of reading it from a file
     val testPartitionSizes = workers.map(_ => {
